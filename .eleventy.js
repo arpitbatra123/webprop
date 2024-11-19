328ef6a978c8fd73dbd8c1278bcbdf996c00d85b
Arpit Batra <arpitbatra123@gmail.com>
2024-11-19 17:18:30 +0530

    upgrade 11ty to 3.0

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,18 @@
 // .eleventy.js
-const markdownIt = require('markdown-it');
-module.exports = function (eleventyConfig) {
+
+import markdownIt from "markdown-it";
+export default function (eleventyConfig) {
   // Watch CSS files for changes
   eleventyConfig.setBrowserSyncConfig({
     files: "./_site/styles/**/*.css",
   });
-  eleventyConfig.addPassthroughCopy('assets');
+  eleventyConfig.addPassthroughCopy("assets");
   const options = {
-    html: true,
-    breaks: true,
-    linkify: false,
-    typographer: true
-  },
-  markdownEngine = markdownIt(options);
+      html: true,
+      breaks: true,
+      linkify: false,
+      typographer: true,
+    },
+    markdownEngine = markdownIt(options);
+  eleventyConfig.setLibrary("md", markdownEngine);
 };
