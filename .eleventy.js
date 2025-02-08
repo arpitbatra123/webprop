e946831764dea454b527666903525faf7e137f64
Arpit Batra <arpitbatra123@gmail.com>
2025-02-08 15:01:31 +0530

    add RSS feed plugin to Eleventy configuration and update dependencies

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,33 @@
 
 import markdownIt from "markdown-it";
 import Shiki from "@shikijs/markdown-it";
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 export default async function (eleventyConfig) {
   // Watch CSS files for changes
   eleventyConfig.setBrowserSyncConfig({
     files: "./_site/styles/**/*.css",
   });
   eleventyConfig.addPassthroughCopy("assets");
+
+  eleventyConfig.addPlugin(feedPlugin, {
+		type: "atom", // or "rss", "json"
+		outputPath: "/feed.xml",
+		collection: {
+			name: "posts", // iterate over `collections.posts`
+			limit: 0,     // 0 means no limit
+		},
+		metadata: {
+			language: "en",
+			title: "Arpit Batra",
+			subtitle: "The personal website and blog of Arpit Batra",
+			base: "https://arpit.tk/",
+			author: {
+				name: "Arpit Batra",
+				email: "", // Optional
+			}
+		}
+	});
+
   const options = {
       html: true,
       breaks: true,
